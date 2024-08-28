1bbaaeaa85fcc4c0feaa660296e08811538196ba
panda6151025 <liamhu0914@gmail.com>
2024-08-28 07:36:34 -0400

    test only api

--- api/index.py
+++ api/index.py
@@ -10,20 +10,20 @@ def index():
 
 @app.route('/upload', methods=['POST'])
 def upload_audio():
-    if 'audio' not in request.files:
-        return 'No audio file uploaded', 400
+    # if 'audio' not in request.files:
+    #     return 'No audio file uploaded', 400
 
-    audio_file = request.files['audio']
-    if audio_file.filename == '':
-        return 'No audio file selected', 400
+    # audio_file = request.files['audio']
+    # if audio_file.filename == '':
+    #     return 'No audio file selected', 400
 
-    if audio_file:
-        # filename = audio_file.filename
-        # file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        # audio_file.save(file_path)
-        return f'Audio chunk {filename} uploaded successfully'
+    # if audio_file:
+    #     filename = audio_file.filename
+    #     file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    #     audio_file.save(file_path)
+    #     return f'Audio chunk {filename} uploaded successfully'
 
-    return 'Error uploading audio chunk', 500
+    return f'Error uploading audio chunk'
 
 if __name__ == '__main__':
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
